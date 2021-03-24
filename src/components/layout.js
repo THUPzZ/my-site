fbb23f4eba3efe971bb81b27309a929449137629
ทับบัคกระจุย <theem571@epicc.postbox.in.th>
2021-03-24 12:53:29 +0700

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,12 @@ const Layout = ({ location, title, children }) => {
 
   if (isRootPath) {
     header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+      // <h1 className="main-heading">
+      //   <Link to="/">{title}</Link>
+      // </h1>
+      <div style={{ backgroundColor: 'red', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+        <Link style={{ display: 'flex', alignItems: 'center' }} to="/"> THEERAPRASED </Link>
+      </div>
     )
   } else {
     header = (
@@ -21,14 +24,11 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+    <div data-is-root-path={isRootPath}>
+      <header className="global-header">
+        { header }
+      </header>
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
     </div>
   )
 }
